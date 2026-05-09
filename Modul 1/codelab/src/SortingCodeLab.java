77ed9456ca6c16596f6022b6cbcfc93931b75e79
Machobotak <mibrahimalayubi@gmail.com>
2026-05-09 11:19:42 +0700

    Add Module 1 CodeLab

--- Modul 1/codelab/src/SortingCodeLab.java
+++ Modul 1/codelab/src/SortingCodeLab.java
@@ -1,76 +1,75 @@
 public class SortingCodeLab {
-    public class SortingCodeLab {
+    // =========================================
+    // BUBBLE SORT DESCENDING
+    // =========================================
+    static void bubbleSortDescending(int arr[]) {
+        int n = arr.length;
+        boolean swapped;
 
-        // =========================================
-        // BUBBLE SORT DESCENDING
-        // =========================================
-        static void bubbleSortDescending(int arr[]) {
-            int n = arr.length;
-            boolean swapped;
+        for (int i = 0; i < ____; i++) {
+            swapped = false;
 
-            for (int i = 0; i < ____; i++) {
-                swapped = false;
+            // Compare adjacent elements
+            for (int j = 0; j < ____; j++) {
 
-                // Compare adjacent elements
-                for (int j = 0; j < ____; j++) {
+                // Swap if left element is smaller
+                if (arr[j] ____ arr[j + 1]) {
 
-                    // Swap if left element is smaller
-                    if (arr[j] ____ arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
 
-                        int temp = arr[j];
-                        arr[j] = arr[j + 1];
-                        arr[j + 1] = temp;
-
-                        swapped = true;
-                    }
-                }
-
-                // Stop early if already sorted
-                if (!____) {
-                    break;
+                    swapped = true;
                 }
             }
+
+            // Stop early if already sorted
+            if (!____) {
+                break;
+            }
         }
+    }
 
-        // =========================================
-        // SELECTION SORT DESCENDING
-        // =========================================
-        static void selectionSortDescending(int arr[]) {
-            int n = arr.length;
+    // =========================================
+    // SELECTION SORT DESCENDING
+    // =========================================
+    static void selectionSortDescending(int arr[]) {
+        int n = arr.length;
 
-            for (int i = 0; i < ____; i++) {
+        for (int i = 0; i < ____; i++) {
 
-                // Assume current index is maximum
-                int maxIndex = ____;
+            // Assume current index is maximum
+            int maxIndex = ____;
 
-                // Find the largest element
-                for (int j = ____; j < n; j++) {
+            // Find the largest element
+            for (int j = ____; j < n; j++) {
 
-                    if (arr[j] ____ arr[maxIndex]) {
-                        maxIndex = j;
-                    }
+                if (arr[j] ____ arr[maxIndex]) {
+                    maxIndex = j;
                 }
-
-                // Swap elements
-                int temp = arr[maxIndex];
-                arr[maxIndex] = arr[i];
-                arr[i] = temp;
             }
-        }
 
-        // =========================================
-        // PRINT ARRAY
-        // =========================================
-        static void printArray(int arr[]) {
-            for (int num : arr) {
-                System.out.print(num + " ");
-            }
-            System.out.println();
+            // Swap elements
+            int temp = arr[maxIndex];
+            arr[maxIndex] = arr[i];
+            arr[i] = temp;
         }
+    }
 
-        // =========================================
-        // MAIN PROGRAM
-        // =========================================
+    // =========================================
+    // PRINT ARRAY
+    // =========================================
+    static void printArray(int arr[]) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    // =========================================
+    // MAIN PROGRAM
+    // =========================================
+    public class Main{
         public static void main(String[] args) {
 
             int[] bubbleData = {5, 1, 4, 2, 8, 3, 7, 6, 9, 0};
@@ -91,4 +90,6 @@ public class SortingCodeLab {
             printArray(selectionData);
         }
     }
+
 }
+
